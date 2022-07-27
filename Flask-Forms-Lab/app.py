2d976a3c53ea10dbb8afdd8044bd91c56fd2485b
ShadanAb23-meet <109904715+ShadanAb23-meet@users.noreply.github.com>
2022-07-27 16:31:23 +0300

    j

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for
 import random
 
 app = Flask(  # Create a flask app
-	__name__,
-	template_folder='templates',  # Name of html file folder
-	static_folder='static'  # Name of directory for static files
+    __name__,
+    template_folder='templates',  # Name of html file folder
+    static_folder='static'  # Name of directory for static files
 )
 
 
@@ -20,22 +20,22 @@ def login():
 
 
 
-if __name__ == "__main__":  # Makes sure this is the main process
-	app.run( # Starts the site
-    debug=True
-	)
 
-	@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
     if request.method == 'GET':
         return 'something wrong try again!'
     else:
-        if username == request.form['username'] and password = request.form['password'] :
-        	return render_template('home.html')
+        if username == request.form['username'] and password == request.form['password'] :
+            return render_template('home.html')
 
 
-@app.route('/home/<string:username>')
+@app.route('/home/'<string:username>'')
 def hello(username):
     return render_template(
         'home.html', n = username)
-        	
+    
+if __name__ == "__main__":  # Makes sure this is the main process
+    app.run( # Starts the site
+    debug=True
+    )
